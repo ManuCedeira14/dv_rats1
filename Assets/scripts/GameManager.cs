7c4fb2491c75fbf9e8bfe2abf483dc177d699e70
ManuCedeira14 <manuel.cedeira@davinci.edu.ar>
2024-10-29 11:42:05 -0300

    cambios y ajustes varios

--- Assets/scripts/GameManager.cs
+++ Assets/scripts/GameManager.cs
@@ -4,20 +4,22 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.EventSystems;
 
-public class GameManager : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+public class GameManager : MonoBehaviour/*, IPointerDownHandler, IPointerUpHandler*/
 {
-    Player player;
-    winStar winStar;
+    public Material dmgShader;
+    private const string BoolParameterName = "_on_off";
     public void NextScene()
     {
         SceneManager.LoadSceneAsync(1);
+        dmgShader.SetFloat(BoolParameterName, 0.0f);
+        Debug.Log("hiciste click");
     }
     
-    public void OnPointerDown(PointerEventData eventData)
-    {
-        NextScene();
-    }
-    public void OnPointerUp(PointerEventData eventData)
-    { 
-    }
+    //public void OnPointerDown(PointerEventData eventData)
+    //{
+    //    NextScene();
+    //}
+    //public void OnPointerUp(PointerEventData eventData)
+    //{ 
+    //}
 }
