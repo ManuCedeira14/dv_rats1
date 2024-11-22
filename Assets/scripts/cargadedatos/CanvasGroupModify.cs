7a5d172eb7c7805f1db1b93eee0b55c713e8376b
ManuCedeira14 <manuel.cedeira@davinci.edu.ar>
2024-11-22 20:25:05 -0300

    Revert "1"
    This reverts commit eb794389728bd0101aca4128054abf4c3b460747, reversing
    changes made to 02c1bf7da6579cd4fbf638efe90b97882bfd2f1d.

--- Assets/scripts/cargadedatos/CanvasGroupModify.cs
+++ Assets/scripts/cargadedatos/CanvasGroupModify.cs
@@ -4,55 +4,20 @@ using UnityEngine;
 
 public class CanvasGroupModify : MonoBehaviour
 {
-    [SerializeField] private Canvas menuPrincipal;
-    [SerializeField] private Canvas pausa;
-    [SerializeField] private Canvas options;
-    [SerializeField] private CanvasGroup shop;
+    [SerializeField] CanvasGroup _myCanvasGroup;
 
     private void Start()
     {
-        ShowMenu();
+        ShowOrHideMenu(false);
 
     }
-    public void ShowMenu()
+
+    public void ShowOrHideMenu(bool Value)
     {
-        
-        menuPrincipal.gameObject.SetActive(true);
+        if (Value) _myCanvasGroup.alpha = 1;
+        else _myCanvasGroup.alpha = 0;
 
-        
-        ShowOrHideShop(false);
+        _myCanvasGroup.interactable = Value;
+        _myCanvasGroup.blocksRaycasts = Value;
     }
-
-    public void ShowPause()
-    {
-        menuPrincipal.gameObject.SetActive(false);
-        pausa.gameObject.SetActive(true);
-        options.gameObject.SetActive(false);
-        ShowOrHideShop(false); 
-    }
-
-    public void ShowOptions()
-    {
-        menuPrincipal.gameObject.SetActive(false);
-        pausa.gameObject.SetActive(false);
-        options.gameObject.SetActive(true);
-        ShowOrHideShop(false); 
-    }
-    public void ShowShop()
-    {
-        
-        menuPrincipal.gameObject.SetActive(false);
-
-       
-        ShowOrHideShop(true);
-    }
-
-    private void ShowOrHideShop(bool value)
-    {
-        shop.alpha = value ? 1 : 0;
-        shop.interactable = value;
-        shop.blocksRaycasts = value;
-    }
-
 }
- 
