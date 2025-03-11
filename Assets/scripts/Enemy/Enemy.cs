71a0b94ec0bff45e8c1c3b717b1e2e7c05a25530
ManuCedeira14 <manuel.cedeira@davinci.edu.ar>
2025-03-11 13:16:11 -0300

    gfdgdf

--- Assets/scripts/Enemy/Enemy.cs
+++ Assets/scripts/Enemy/Enemy.cs
@@ -9,20 +9,33 @@ public class Enemy : MonoBehaviour
     [SerializeField] protected float _maxForce;
     protected Vector3 _velocity;
     public Vector3 Velocity { get { return _velocity; } }
-    [SerializeField] private float life = 3f; // Vida inicial
-    [SerializeField] private Material dissolveMaterial; // Material para el shader de disolución
-    [SerializeField] private float dissolveSpeed = 1f; // Velocidad de disolución
-    [SerializeField] private Material originalMaterial; // Material original del enemigo
-    [SerializeField] private Renderer enemyRenderer; // Renderer del enemigo
+    [SerializeField] private float life = 3f;
+    [SerializeField] private Material dissolveMaterial;
+    [SerializeField] private float dissolveSpeed = 1f;
+    [SerializeField] private Material originalMaterial;
+    [SerializeField] private Renderer enemyRenderer;
+    [SerializeField] private ParticleSystem explotionPS;
+    [SerializeField] private ParticleSystem sparksPS;
     private bool isDying = false;
 
+   
+    protected virtual void Awake()
+    {
+        if (explotionPS != null)
+        {
+            explotionPS.Stop(); 
+            sparksPS.Stop(); 
+        }
+    }
 
     protected virtual void Start()
     {
         enemyRenderer = GetComponent<Renderer>();
         originalMaterial = enemyRenderer.material;
+
         _velocity = Vector3.zero;
     }
+
     protected virtual void Update()
     {
         transform.position += _velocity * Time.deltaTime;
@@ -46,6 +59,13 @@ public class Enemy : MonoBehaviour
 
         float dissolveValue = 0f;
         currentDissolveMaterial.SetFloat("_DissolveAmount", dissolveValue);
+
+        
+        if (explotionPS != null)
+        {
+            explotionPS.Play(); 
+            sparksPS.Play(); 
+        }
 
         while (dissolveValue < 1f)
         {
